a2dc4be1f1d47a16abb224b10aa6ebe7eb017c51
python <maksdavid2009@gmail.com>
2023-12-19 22:17:11 +0100

    2

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 class Human:
     __height = 170
-    pole = 100
+    _pole = 100
     group = "?????"
 
-def set_height(self, height):
-    if height > 240:
-        return
-    self.__height = height
+    def set_heigth(self, height):
+        if height > 240:
+            return
+        self.__height = height
 
-def get_height(self):
-    self.__method()
-    return self.__height
-
-
-
-
-
+    def get_height(self):
+        return self.__height
 
     def __method(self):
-        print("method")
-
+        print("Method")
 
 class Student(Human):
     group = "1016"
-
     def __init__(self, height):
-       return self.__height
-
+        self.set_heigth(height)
 
 
 class Worker(Human):
     group = "Builder"
 
-h = Human()
+
+
+'''h = Human()
+print(h.get_height())
+h.set_heigth(175)
 print(h.get_height())
 
+
 stud = Student(160)
-print(stud.height)
+print(stud.get_height())
 print(stud.group)
 
-
 w = Worker()
-print(w.height)
-print(w.group)
+print(w.get_height())
+print(w.group)'''
+
+
+class Comp:
+    def __init__(self):
+        super().__init__()
+        self.memory = 128
+    def calc(self):
+        print("I`m calcing")
+
+class Display:
+    def __init__(self):
+        super().__init__()
+        self.resolution = "2k"
+    def info(self):
+        print("I`m printing info")
+
+
+class Mobile(Comp, Display):
+    def print_info(self):
+        print(self.resolution)
+        print(self.memory)
+
+
+mob = Mobile()
+mob.calc()
+mob.info()
+mob.print_info()
